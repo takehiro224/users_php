e3fbe4b62ac1aeba3513cd92960a022e19d9eeaf
take-watanabe@autobacs.com <take-watanabe@autobacs.com>
2023-08-03 18:54:25 +0900

    DBの初期設定・初期データ投入とログ出力設定

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 docker exec -it web_container bash
+
+## PostgreSQL
+postgres: PostgreSQLの実行ファイルを指定しています。このコマンドを実行することでPostgreSQLサーバーが起動します。
+-c log_destination=stderr: ログの出力先を標準エラー出力（stderr）に設定しています。これにより、PostgreSQLのログがコンテナの標準エラー出力に出力されるようになります。
+-c log_statement=all: 実行されたSQLステートメントをログに記録する設定です。all はすべてのSQLステートメントをログに記録することを意味します。
+-c log_connections=on: データベースへの接続をログに記録する設定です。on は接続のログ記録を有効にすることを意味します。
+-c log_disconnections=on: データベースからの切断をログに記録する設定です。on は切断のログ記録を有効にすることを意味します。
--- db/init/init.sql
+++ db/init/init.sql
@@ -1,8 +1,21 @@
-\c pgsql;
--- テーブル作成
-DROP TABLE IF EXISTS sample;
-CREATE TABLE sample (
-	id integer NOT NULL PRIMARY KEY,
-	name char(100) NOT NULL,
-	created_date_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+-- 下記の「\c データベース名」が書かれていないと実行されない
+\c postgres;
+-- usersテーブルを作成するクエリ
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
+
+-- usersテーブルに初期データを挿入するクエリ
+INSERT INTO users (name, email, password)
+VALUES
+  ('John Doe', 'john@example.com', 'password123'),
+  ('Jane Smith', 'jane@example.com', 'secret123'),
+  ('Bob Johnson', 'bob@example.com', '123456');
+
+
+
+
+
